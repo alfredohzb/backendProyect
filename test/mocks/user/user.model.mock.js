0852342ceaa677c599d6987087c16bb3dd4f8887
Alfredo Hernández Bula <alfredohernandezbula@hotmail.com>
2020-02-25 09:09:48 -0500

    [ADD] test added

---
+++ test/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: "5e551b0d1c9d440000f75c64",
+    name: "Alfredo Hernandez",
+    username: "Alfredo",
+    password: "$2a$10$LvWkhsiCfqKWs2Qr04yayOScUPkpru7NA/CDLn7Zw/IRPewUW08Vi"
+  },
+  users: [
+    {
+      _id: "5e551b0d1c9d440000f75c64",
+      name: "Alfredo Hernandez",
+      username: "Alfredo",
+      password: "$2a$10$LvWkhsiCfqKWs2Qr04yayOScUPkpru7NA/CDLn7Zw/IRPewUW08Vi"
+    },
+    {
+      _id: "5e551b581c9d440000f75c65",
+      name: "Alfredo Testing",
+      username: "Alfredo test",
+      password: "$2a$10$7.2Egqval9BauJWRvR01HOj/7TyBUnCu3MswX6pVyaJ/kHLxyO5aq"
+    }
+  ]
+};
